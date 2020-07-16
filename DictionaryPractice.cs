4e8898c32062af4a06b1534ca489a0e6a336f663
Syed Moiz <syed.m.moiz@gmail.com>
2020-07-15 22:39:49 -0400

    Added Dictionary

---
+++ DictionaryPractice.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace _99_csharp_problems
+{
+    class DictionaryPractice 
+    {
+        static void Main(String[] args) 
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _99_csharp_problems
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
